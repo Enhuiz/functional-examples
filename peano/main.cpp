ececee98012d0c2ae2d03ebe9f34fe7dee70253e
enhuiz <niuzhe.nz@outlook.com>
2017-07-24 14:16:03 +0800

    restore

--- peano/main.cpp
+++ peano/main.cpp
@@ -1,80 +1,80 @@
 #include <iostream>
-#include <type_traits>
+#include <typeinfo>
 
-struct Zero;
+namespace peano
+{
+struct zero
+{
+    using type = zero;
+};
 
 template <typename N>
-struct Successor;
+struct successor
+{
+    using type = successor;
+};
 
 template <typename N, typename M>
 struct plus;
 
 template <typename N>
-struct plus<N, Zero>
+struct plus<N, zero>
 {
     using type = N;
 };
 
 template <typename N, typename M>
-struct plus<N, Successor<M>>
+struct plus<N, successor<M>>
 {
-    using type = Successor<typename plus<N, M>::type>;
+    using type = successor<typename plus<N, M>::type>;
 };
 
-// helpers
-struct NaturalError;
-
-template <bool C, typename Then, typename Else>
-struct if_ {
-    using Branch = typename std::conditional<C, Then, Else>::type;
-    using type = Branch; // Note that we don't unbox Branch here
-};
-
-template <int N>
+template <int N, bool Valid = N >= 0>
 struct int_to_natural;
 
 template <>
 struct int_to_natural<0>
 {
-    using type = Zero;
+    using type = zero;
 };
 
 template <int N>
-struct int_to_natural
+struct int_to_natural<N, true>
 {
-    using type = if_<N > 0, int_to_natural<N - 1>, NaturalError>;
+    using type = successor<typename int_to_natural<N - 1>::type>;
 };
 
-// template <int N>
-// struct int_to_natural<N, false>
-// {
-//     static_assert(N >= 0, "A Natural number must be a non-negative Integer.");
-// };
+template <int N>
+struct int_to_natural<N, false>
+{
+    static_assert(N >= 0, "A Natural number must be a non-negative Integer.");
+};
 
 template <typename N>
 struct natural_to_int;
 
 template <>
-struct natural_to_int<Zero>
+struct natural_to_int<zero>
 {
     static constexpr int value = 0;
 };
 
 template <typename N>
-struct natural_to_int<Successor<N>>
+struct natural_to_int<successor<N>>
 {
     static constexpr int value = 1 + natural_to_int<N>::value;
 };
+}
 
-// main
 int main()
 {
-    using five = int_to_natural<5>::type;
-    using ten = int_to_natural<10>::type;
-    using sum = plus<five, ten>::type;
-    int value = natural_to_int<sum>::value;
+    using five = peano::int_to_natural<5>::type;
+    using ten = peano::int_to_natural<10>::type;
+    using sum = peano::plus<five, ten>::type;
+    int value = peano::natural_to_int<sum>::value;
 
     std::cout << value << std::endl;
+    std::cout << typeid(sum()).name() << std::endl;
 
     return 0;
 }
